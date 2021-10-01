f8b5f843881d5e601af1f3ba04f7a7d1aaeda135
millie_tsai <millie_tsai@me.com>
2021-09-30 22:35:21 -0700

    updated js and some files

---
+++ Develop/index.js
@@ -0,0 +1,137 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const { title } = require('process');
+
+
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [ 
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Describe the installation process if any:",
+        name: 'installation'
+    },
+    { 
+    type: 'input',
+    message: "What is this project usage for?",
+    name: 'usage'
+    },
+   
+    {
+        type: 'input',
+        message: "Who are the contributors of this projects?",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "Is there a test included?",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    },
+ 
+    {
+    type: 'input',
+    message: "What is your GitHub username? (No @ needed)",
+    name: 'username',
+    default: 'connietran-dev',
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid GitHub username is required.");
+        }
+        return true;
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+},
+
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
+// TODO: Create a function to initialize app
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+// Function call to initialize app
+init();
+
+
+
+
+
+
+
+
